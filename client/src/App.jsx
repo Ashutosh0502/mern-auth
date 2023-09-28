44d51d63c9b6c02c33ad92b5e5e48c3e23061d7a
Your Name <you@example.com>
2023-09-28 14:35:22 +0530

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className='text-red-500'>App</div>
-  )
+  return <BrowserRouter>
+    
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+    
+  </BrowserRouter>
 }
 
 export default App
